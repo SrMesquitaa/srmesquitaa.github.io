eb6571b1270be2603d40dbf3da4bee5942f58b7a
Leonardo Mesquita <contatoleonaardo@gmail.com>
2025-03-29 10:23:00 -0300

    Primeiro commit do portfólio

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const fadeInSections = document.querySelectorAll(".fade-in");
+
+  function checkScroll() {
+      fadeInSections.forEach((section) => {
+          const sectionTop = section.getBoundingClientRect().top;
+          if (sectionTop < window.innerHeight * 0.9) {
+              section.classList.add("show");
+          }
+      });
+  }
+
+  window.addEventListener("scroll", checkScroll);
+  checkScroll();
+});
