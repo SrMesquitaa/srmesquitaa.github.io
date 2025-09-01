87ea9d61d558b103f0f66086ec97bfb6cfa85126
Leonardo Mesquita <contatoleonaardo@gmail.com>
2025-09-01 10:50:45 -0300

    atualizações botão de linguagem, novo dashboard de BI de analise da Uber e outras features

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const fadeInSections = document.querySelectorAll(".fade-in");
+    const fadeInSections = document.querySelectorAll(".fade-in");
 
-  function checkScroll() {
-      fadeInSections.forEach((section) => {
-          const sectionTop = section.getBoundingClientRect().top;
-          if (sectionTop < window.innerHeight * 0.9) {
-              section.classList.add("show");
-          }
-      });
-  }
+    function checkScroll() {
+        fadeInSections.forEach((section) => {
+            const sectionTop = section.getBoundingClientRect().top;
+            if (sectionTop < window.innerHeight * 0.9) {
+                section.classList.add("show");
+            }
+        });
+    }
 
-  window.addEventListener("scroll", checkScroll);
-  checkScroll();
+    window.addEventListener("scroll", checkScroll);
+    checkScroll();
 });
